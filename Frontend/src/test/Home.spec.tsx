462a9aa91de9f0838f8b9ea734bbcbd08fd04fa8
Carlos Rosa <carlosrosa1001@gmail.com>
2023-01-13 14:02:32 -0300

    test: :white_check_mark: Home page tests

---
+++ Frontend/src/test/Home.spec.tsx
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import Login from "../pages/Login";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+import { renderWithRouter } from "./renderWithRouter";
+
+describe("Home page tests", () => {
+  it("Should Home render elements", async () => {
+    renderWithRouter(<App />);
+
+    const usernameInput = screen.getByText(/Username/i);
+    const passwordInput = screen.getByText(/password/i);
+
+    userEvent.type(usernameInput, "Rosa");
+    userEvent.type(passwordInput, "Rosapass1");
+
+    const loginButton = screen.getByRole("button", { name: "Login" });
+    await userEvent.click(loginButton);
+
+    const user = await screen.findAllByText(/Rosa/i);
+    const tabela = await screen.findByText(/Historico de Transações/i);
+    const saldo = await screen.findByText(/Saldo:/i);
+    const transferir = await screen.findByText(/Transferir/i);
+
+    expect(user[0]).toBeInTheDocument();
+    expect(tabela).toBeInTheDocument();
+    expect(saldo).toBeInTheDocument();
+    expect(transferir).toBeInTheDocument();
+  });
+
+  it("Should Home make transferencia", async () => {
+    renderWithRouter(<App />);
+
+    const usernameInput = screen.getByText(/Username/i);
+    const passwordInput = screen.getByText(/password/i);
+
+    userEvent.type(usernameInput, "Rosa");
+    userEvent.type(passwordInput, "Rosapass1");
+
+    const loginButton = screen.getByRole("button", { name: "Login" });
+    await userEvent.click(loginButton);
+
+    const valueInput = await screen.findByPlaceholderText("Value");
+    const userInput = await screen.findByPlaceholderText("@Username");
+    userEvent.type(valueInput, "10");
+    userEvent.type(userInput, "Carlos");
+
+    const enviarButton = await screen.findByRole("button", { name: "Enviar" });
+    await userEvent.click(enviarButton);
+
+    const elementTable = await screen.findAllByRole("row");
+    console.log("AQUI-----------------", elementTable);
+
+    expect(elementTable[0]).toBeInTheDocument();
+  });
+});
