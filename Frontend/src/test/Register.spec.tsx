42ab4b7b48d29cb7b1a80155a51742fe9c666dfb
Carlos Rosa <carlosrosa1001@gmail.com>
2023-01-13 13:43:34 -0300

    test: :white_check_mark: Register page tests

---
+++ Frontend/src/test/Register.spec.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import Login from "../pages/Login";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+import { renderWithRouter } from "./renderWithRouter";
+
+describe("Register page tests", () => {
+  it("Should Register render elements", () => {
+    renderWithRouter(<App />, { route: "/register" });
+
+    const mainTitle = screen.getByText(/Register/i);
+    const usernameInput = screen.getByText(/Username/i);
+    const passwordInput = screen.getByText(/Password/i);
+    const createButton = screen.getByRole("button", { name: /Create/i });
+
+    expect(mainTitle).toBeInTheDocument();
+    expect(usernameInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(createButton).toBeInTheDocument();
+  });
+
+  it("Should register a user", async () => {
+    /* 
+    renderWithRouter(<App />, { route: "/register" });
+    
+    const usernameInput = screen.getByText(/Username/i);
+    const passwordInput = screen.getByText(/Password/i);
+    
+    userEvent.type(usernameInput, "NewUser");
+    userEvent.type(passwordInput, "NewUserPass1");
+    
+    const createButton = screen.getByRole("button", { name: /Create/i });
+    
+    await userEvent.click(createButton);
+    
+    const initialBalance = await screen.findByText(/Saldo: R$ 100/i);
+    
+    expect(initialBalance).toBeInTheDocument();
+    */
+  });
+
+  it("Should not register a user with invalid inputs", async () => {
+    renderWithRouter(<App />, { route: "/register" });
+
+    const usernameInput = screen.getByText(/Username/i);
+    const passwordInput = screen.getByText(/Password/i);
+
+    userEvent.type(usernameInput, "Invalid");
+    userEvent.type(passwordInput, "InvalidPass");
+
+    const createButton = screen.getByRole("button", { name: /Create/i });
+
+    await userEvent.click(createButton);
+
+    const initialBalance = screen.queryByText(/Saldo: R$ 100/i);
+
+    expect(initialBalance).not.toBeInTheDocument();
+  });
+});
